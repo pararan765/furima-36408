ccd1f1159e6a5af04d15d61a7238e6c1a35f5f5a
原健太 <harakenta@harakentanoMacBook-Air.local>
2021-08-18 14:36:18 +0900

    ユーザー管理機能の再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do 
-  validates :nickname
-  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-  validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "カタカナを使用してください"}
-  validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "カタカナを使用してください"}
-  validates :birthday
-  validates :password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+   validates :nickname
+   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+     validates :last_name 
+     validates :first_name
+   end
+   with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "カタカナを使用してください"} do
+     validates :last_name_kana 
+     validates :first_name_kana
+   end
+   validates :birthday
+   validates :password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
     it 'パスワードを６文字以上記入したら登録できること' do
-      @user.password = "kkkkkk"
-      @user.password_confirmation = "kkkkkk"
+      @user.password = "kkkkkk12"
+      @user.password_confirmation = "kkkkkk12"
       expect(@user).to be_valid
     end
   end
@@ -32,6 +32,21 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
+    it 'パスワードが半角英語のみの場合登録できない' do
+      @user.password = 'rebornnewworld'
+      @user.password_confirmation = 'rebornnewworld'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
+
+    it 'パスワードが数字のみの場合登録できない' do
+      @user.password = '1234567'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
+
+    end
     it 'emailが重複する場合、新規登録ができない' do
       @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
@@ -50,7 +65,60 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email is invalid")
     end
-  end
 
- end
-end
+    it '誕生日が空の場合登録できない' do
+      @user.birthday = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
+    end
+
+    it '姓が空白の場合登録できない' do
+      @user.last_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it '姓が全角以外の場合登録できない' do
+      @user.last_name = "matsue"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name 全角文字を使用してください")
+      
+    end
+
+    it '名が空白の場合登録できない' do
+      @user.first_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it '名が全角以外の場合登録できない' do
+      @user.first_name = "matsue"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name 全角文字を使用してください")
+    end
+
+    it '姓（フリガナ）が空白の場合登録できない' do
+      @user.last_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+    end
+
+    it '姓（フリガナ）が全角以外の場合登録できない' do
+      @user.last_name_kana = "matsue"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana カタカナを使用してください")
+    end
+
+    it '名（フリガナ）が空白の場合登録できない' do
+      @user.first_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
+    end
+
+    it '名（フリガナ）が全角以外の場合登録できない' do
+      @user.first_name_kana = "matsue"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana カタカナを使用してください")
+    end
+  end
+ end 

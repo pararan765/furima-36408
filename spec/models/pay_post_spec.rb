a2dd7bdf4724f9ae3e40854e3f26d1cac36e0f20
原健太 <harakenta@harakentanoMacBook-Air.local>
2021-08-28 16:49:19 +0900

    Rspecの再修正

--- spec/models/pay_post_spec.rb
+++ spec/models/pay_post_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '商品購入' do
     before do
-      user = FactoryBot.create(:user)
-      item = FactoryBot.create(:item)
-      @pay_post = FactoryBot.build(:pay_post, user_id: user_id, item_id: item_id)
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.create(:item)
+      @pay_post = FactoryBot.build(:pay_post, user_id: @user.id, item_id: @item.id)
+      sleep(1)
     end
 
     context '商品を購入できる' do
